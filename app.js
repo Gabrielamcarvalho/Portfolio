42e876ffc383491079b6921d10bb3542d510ce1b
Gabriela Melnik Carvalho <gabrielamcarvalho@gmail.com>
2021-08-31 11:50:00 +0100

    added landing pages

--- app.js
+++ app.js
@@ -1,65 +1,57 @@
-const projectsContainer = document.querySelector('projects-container');
-const app = document.querySelectorAll('.app');
-const landingPage = document.querySelectorAll('.landingPage');
-const games = document.querySelectorAll('.game');
+const landingPages = [
+  {
+    name: 'Yo.Design',
+    img: './images/Yodesign.jpg',
+    liveLink: 'https://yodesign.netlify.app/',
+    code: 'https://github.com/Gabrielamcarvalho/YOdesign',
+  },
+  {
+    name: 'Mountairs',
+    img: './images/Mountairs.jpg',
+    liveLink: 'https://mountairs.netlify.app/',
+    code: 'https://github.com/Gabrielamcarvalho/Mountairs',
+  },
+  {
+    name: 'Dunder Mifflin',
+    img: './images/dunder.jpg',
+    liveLink: 'https://gabrielamcarvalho.github.io/DunderMifflin/index.html',
+    code: 'https://github.com/Gabrielamcarvalho/DunderMifflin',
+  },
+];
 
-const changeCategory = () => {
-  if (document.getElementById('apps').selected) {
-    app.forEach((a) => {
-      a.classList.remove('hidden');
-      a.classList.add('show');
-    });
-    landingPage.forEach((l) => {
-      l.classList.remove('show');
-      l.classList.add('hidden');
-    });
-    games.forEach((g) => {
-      g.classList.remove('show');
-      g.classList.add('hidden');
-    });
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
-    console.log('apps');
-  } else if (document.getElementById('landing-pages').selected) {
-    app.forEach((a) => {
-      a.classList.remove('show');
-      a.classList.add('hidden');
-    });
-    landingPage.forEach((l) => {
-      l.classList.remove('hidden');
-      l.classList.add('show');
-    });
-    games.forEach((g) => {
-      g.classList.remove('show');
-      g.classList.add('hidden');
-    });
+const landingPageName = document.querySelector('.landing-page--name');
+const landingPageImg = document.querySelector('.landing-page--src');
+const landingPageLive = document.querySelector('.btn--view-live');
+const landingPageCode = document.querySelector('.btn--view-code');
 
-    console.log('landing page');
-  } else if (document.getElementById('games').selected) {
-    app.forEach((a) => {
-      a.classList.remove('show');
-      a.classList.add('hidden');
-    });
-    landingPage.forEach((l) => {
-      l.classList.remove('show');
-      l.classList.add('hidden');
-    });
-    games.forEach((g) => {
-      g.classList.remove('hidden');
-      g.classList.add('show');
-    });
-    console.log('games');
-  } else {
-    app.forEach((a) => {
-      a.classList.remove('hidden');
-      a.classList.add('show');
-    });
-    landingPage.forEach((l) => {
-      l.classList.remove('hidden');
-      l.classList.add('show');
-    });
-    games.forEach((g) => {
-      g.classList.remove('hidden');
-      g.classList.add('show');
-    });
-  }
+let currentLandingPage = 0;
+
+const displayLandingPage = () => {
+  landingPageName.textContent = `${landingPages[currentLandingPage].name} - Responsive Landing Page `;
+  landingPageImg.src = landingPages[currentLandingPage].img;
+  landingPageLive.href = landingPages[currentLandingPage].liveLink;
+  landingPageCode.href = landingPages[currentLandingPage].code;
 };
+
+displayLandingPage();
+
+prevBtn.addEventListener('click', () => {
+  currentLandingPage--;
+
+  if (currentLandingPage < 0) currentLandingPage = landingPages.length - 1;
+
+  console.log(currentLandingPage);
+  displayLandingPage();
+});
+
+nextBtn.addEventListener('click', () => {
+  currentLandingPage++;
+
+  if (currentLandingPage > landingPages.length - 1) currentLandingPage = 0;
+
+  console.log(currentLandingPage);
+  displayLandingPage();
+});
